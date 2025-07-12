c554a9189988c8555a8481405d7470b6428b6773
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-07-12 08:38:38 -0300

    feat: add dish management with form and admin pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 import AdministacaoRestaurantes from './paginas/Administracao/Restaurantes/AdministacaoRestaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
+import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
+import FormularioPratos from './paginas/Administracao/Pratos/FormularioPratos';
 
 function App() {
 
@@ -15,7 +17,10 @@ function App() {
       <Route path="/admin" element={<PaginaBaseAdmin />} >
         <Route path="restaurantes" element={<AdministacaoRestaurantes />} />
         <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
-        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />   
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />  
+
+        <Route path="pratos" element={<AdministracaoPratos />} />  
+        <Route path="pratos/novo" element={<FormularioPratos />} />  
       </Route>
 
     </Routes>
